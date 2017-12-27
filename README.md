0359e0631b59a84e5d6b688bba230568a2d73f48
err0r10 <dimanah10@gmail.com>
2017-12-27 08:52:15 -0800

    implemetation function load_data and pretty_print_json

--- README.md
+++ README.md
@@ -10,11 +10,10 @@ Example of script launch on Linux, Python 3.5:
 
 ```bash
 
-$ python pprint_json.py <path to file>
+$ python pprint_json.py 4.json
 # TODO add output example
-
 ```
 
 # Project Goals
-
+Print content json file
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,30 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    try:
+        f = open(filepath)
+        return f.read()
+    except FileNotFoundError:
+        print('File not found')
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(json.loads(data), indent=4)
+
+
+if len(sys.argv) == 2:
+    data = load_data(sys.argv[1])
+    if data:
+        print(pretty_print_json(data))
+
+else:
+    print('Run from command line and set one parameter, example: python pprint_json.py 4.json')
+    sys.exit(1)
 
 
 if __name__ == '__main__':
     pass
+    #data = load_data('4.json')
+    #print(pretty_print_json(data))
