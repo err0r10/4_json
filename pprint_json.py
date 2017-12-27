3f59a2cae6585acbbf96ea454c9499f611c54dea
err0r10 <dimanah10@gmail.com>
2017-12-27 09:22:45 -0800

    change name variable

--- pprint_json.py
+++ pprint_json.py
@@ -4,20 +4,20 @@ import sys
 
 def load_data(filepath):
     try:
-        f = open(filepath)
-        return f.read()
+        file_discriprion = open(filepath)
+        return file_discriprion.read()
     except FileNotFoundError:
         print('File not found')
 
 
-def pretty_print_json(data):
-    return json.dumps(json.loads(data), indent=4)
+def pretty_print_json(data_json):
+    return json.dumps(json.loads(data_json), indent=4)
 
 
 if len(sys.argv) == 2:
-    data = load_data(sys.argv[1])
+    data_json = load_data(sys.argv[1])
     if data:
-        print(pretty_print_json(data))
+        print(pretty_print_json(data_json))
 
 else:
     print('Run from command line and set one parameter, example: python pprint_json.py 4.json')
@@ -26,5 +26,3 @@ else:
 
 if __name__ == '__main__':
     pass
-    #data = load_data('4.json')
-    #print(pretty_print_json(data))
