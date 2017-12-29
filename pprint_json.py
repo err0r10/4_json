94b475bdda9610f2dbb5c0c43ab411eda13fa8e7
err0r10 <dimanah10@gmail.com>
2017-12-29 10:07:11 +0300

    refactoring code and add function check_data

--- pprint_json.py
+++ pprint_json.py
@@ -1,28 +1,34 @@
 import json
 import sys
+from os.path import isfile
 
 
-def load_data(filepath):
-    try:
-        file_discriprion = open(filepath)
-        return file_discriprion.read()
-    except FileNotFoundError:
+def read_file(path_file):
+    with open(path_file) as file_description:
+        return file_description.read()
+
+
+def check_data(json_data):
+    return 1 if len(json_data) else None
+
+
+def format_data(json_data):
+    return json.dumps(json.loads(json_data), indent=4,  ensure_ascii=False)
+
+
+def print_json(path_file):
+    if isfile(path_file):
+        _read_data = read_file(path_file)
+        if check_data(_read_data):
+            print(format_data(_read_data))
+        else:
+            print('File is empty')
+    else:
         print('File not found')
 
 
-def pretty_print_json(data_json):
-    return json.dumps(json.loads(data_json), indent=4)
-
-
-if len(sys.argv) == 2:
-    data_json = load_data(sys.argv[1])
-    if data:
-        print(pretty_print_json(data_json))
-
-else:
-    print('Run from command line and set one parameter, example: python pprint_json.py 4.json')
-    sys.exit(1)
-
-
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 2:
+        print_json(sys.argv[1])
+    else:
+        print('Run from command line and set one parameter, example: python pprint_json.py test.json')
