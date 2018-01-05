7925b1f97b742e3e5425e49a3c2ed8b9764fdc6c
err0r10 <dimanah10@gmail.com>
2018-01-05 23:11:24 +0300

    change for pep8

--- pprint_json.py
+++ pprint_json.py
@@ -9,7 +9,7 @@ def read_file(path_file):
 
 
 def check_data(content):
-    return 1 if len(content) else None
+    return 1 if content else None
 
 
 def prettify(content):
@@ -22,13 +22,16 @@ def print_json(path_file):
         if check_data(content_json):
             return prettify(content_json)
         else:
-            return 'File is empty'
+            return "File is empty"
     else:
-        return 'File not found'
+        return "File not found"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) == 2:
         print(print_json(sys.argv[1]))
     else:
-        print('Run from command line and set one parameter, example: python pprint_json.py test.json')
+        print(
+            "Run from command line and set one parameter, "
+            "example: python pprint_json.py test.json"
+        )
