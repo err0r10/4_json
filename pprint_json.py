d0efbcfccb6c080675c8f9770759c81c0394ac73
err0r10 <dimanah10@gmail.com>
2018-01-06 16:33:34 +0300

    add exception in function print_json

--- pprint_json.py
+++ pprint_json.py
@@ -1,41 +1,34 @@
 import json
 import sys
-from os.path import isfile
 
 
-def read_file(path_file):
-    with open(path_file) as desc:
-        return desc.read()
-
-
-def check_data(content):
-    return True if content else False
+def read_file(filename):
+    with open(filename) as target_file:
+        return target_file.read()
 
 
 def prettify(content):
     return json.dumps(
-                json.loads(content),
-                indent=4,
-                ensure_ascii=False
+        json.loads(content),
+        indent=4,
+        ensure_ascii=False
     )
 
 
-def print_json(path_file):
-    if isfile(path_file):
-        content_json = read_file(path_file)
-        if check_data(content_json):
-            return prettify(content_json)
+def print_json(filename):
+    try:
+        source_file = read_file(filename)
+        if source_file:
+            print(prettify(source_file))
         else:
-            return "File is empty"
-    else:
-        return "File not found"
+            print("File '{0}' empty".format(filename))
+    except FileNotFoundError as not_found:
+        print("File '{0}' not found".format(not_found.filename))
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        print(
-            print_json(sys.argv[1])
-        )
+        print_json(sys.argv[1])
     else:
         print(
             "Run from command line and set one parameter, "
